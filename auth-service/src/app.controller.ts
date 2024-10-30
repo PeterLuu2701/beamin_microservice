5617af39e914e7d4c932873b4957c5b8c5a00f0f
PeterLuu2701 <longluuduchoanglong@gmail.com>
2024-10-29 22:41:02 -0600

    update

--- auth-service/src/app.controller.ts
+++ auth-service/src/app.controller.ts
@@ -7,13 +7,7 @@ export class AppController {
   constructor(private readonly appService: AppService) {}
 
   @MessagePattern('login-user')
-  async login(@Body() loginDto: { user_name: string; password: string }) {
-    const user = await this.appService.validateUser(loginDto.user_name, loginDto.password);
-
-    if (user) {
-      return this.appService.login(user);
-    } else {
-      throw new UnauthorizedException('Invalid credentials');
-    }
+  login(@Body() user) {
+    return this.appService.login(user);
   }
 }
--- auth-service/src/app.service.ts
+++ auth-service/src/app.service.ts
@@ -4,20 +4,29 @@ import { JwtService } from '@nestjs/jwt';
 
 @Injectable()
 export class AppService {
-  constructor(private jwtService: JwtService) {}
-
-  async validateUser(user_name: string, password: string): Promise<any> {
-    const user = { user_name, password };
-    if (user.user_name === user_name && user.password === password) {
-      return user;
-    }
-    return null;
-  }
+  constructor(
+    private prismaService: PrismaService,
+    private jwtService: JwtService,
+  ) {}
 
   async login(user: any) {
-    const payload = { user_name: user.user_name, user_id: user.id };
-    return {
-      access_token: this.jwtService.sign(payload),
-    };
+    let { user_name, password } = user;
+
+    let checkUser = await this.prismaService.users.findFirst({
+      where: {
+        user_name,
+        password,
+      },
+    });
+
+    if (checkUser){
+      let token = this.jwtService.sign({ userId: checkUser.id}, {
+          expiresIn: '1d',
+          secret: 'token'
+      })
+      return token
+  } else {
+      return 'Wrong email or password'
+  }
   }
 }
